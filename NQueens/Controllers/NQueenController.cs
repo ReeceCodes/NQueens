c1c1da364fc809aa90c618a7e0f97124fa74cf9a
Reece Hebel <reecehebel@yahoo.com>
2016-11-02 13:38:27 -0500

    Add project files.

---
+++ NQueens/Controllers/NQueenController.cs
@@ -0,0 +1,29 @@
+﻿using NQueens.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace NQueens.Controllers
+{
+    public class NQueenController : SurfaceController
+    {
+        [ChildActionOnly]
+        public ActionResult GetNQ()
+        {
+            return PartialView("ViewNQView", (new NQ()));
+
+        }
+
+        [NotChildAction]
+        public ActionResult GetNQNum(int numberofqueens)
+        {
+            NQ tempNQ = new NQ(numberofqueens);
+
+            return PartialView("ViewNQView", tempNQ);
+
+        } 
+    }
+}
---
+++ NQueens/Models/NQ.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NQueens.Models
+{
+    public class NQ
+    {
+        public int NumberOfQueens { get; set; }
+
+        public int iterations = 0;
+  
+        public int[] position;
+
+        public List<string> Solutions = new List<string>();
+
+        public NQ()
+        {
+
+        }
+
+        public NQ(int i)
+        {
+            NumberOfQueens = i;
+
+            position = new int[i];
+
+            //recursive, seemingly brute force solution, works ok for 12 Q but after that soooooooo slow. by the time you get to 15 it's almost 175 million iterations. 
+            //upside: gets all valid solutions :) includes where the board is just rotated but the positions are the 'same'
+            //it is actually the display of all the solutions that is slower.
+            //using this you would run out of memory trying to store all possible results and the counter would overflow unless changed to bigint.
+            solve(0);          
+        }
+
+        public bool isSafe(int k, int p)
+        {
+            for (int i = 1; i <= k; i++)
+            {
+                int other = position[k - i];
+                if (other == p || other == p - i || other == p + i)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void solve(int k)
+        {
+            iterations++;
+
+            if (k == NumberOfQueens)
+            {
+                Solutions.Add(string.Join(",",position));
+            }
+            else
+            {
+                for (int p = 0; p < NumberOfQueens; p++)
+                {
+                    if (isSafe(k, p))
+                    {
+                        position[k] = p;
+                        solve(k + 1);
+                    }
+                }
+            }
+        }
+
+    }
+}
